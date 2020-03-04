8edfffa4cedc3369a53fd56d06d5bc194a4b6f0e
Kushagra Agrawal <57929451+kushagra7176@users.noreply.github.com>
2020-03-03 20:57:53 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,30 +1,30 @@
 import pygame as pg
 
 
-def getMap(image,height,rigid):
+def getMap(image,height):
     blue = (0, 0, 255)
     white = (255, 255, 255)
     image.fill(white)
-    coord_Polygon = [(20 - rigid, height - (120 - rigid)),
-                  (25 - rigid, height - (185 + rigid)),
-                  (75 + rigid, height - (185 + rigid)),
-                  (100 + rigid, height - (150 + rigid)),
-                  (75 + rigid, height - (120 - rigid)),
-                  (50 + rigid, height - (150 - rigid))]
-    coord_rectangle = [(30 - rigid, height - (67.5 + rigid)),
-                  (35 + rigid, height - (76 + rigid)),
-                  (100 + rigid, height - (38.6 -rigid)),
-                  (95 - rigid, height - (30 - rigid))]
+    coord_Polygon = [(20 , height - (120 )),
+                  (25 , height - (185 )),
+                  (75 , height - (185 )),
+                  (100 , height - (150 )),
+                  (75 , height - (120 )),
+                  (50 , height - (150 ))]
+    coord_rectangle = [(30 , height - (67.5 )),
+                  (35 , height - (76)),
+                  (100 , height - (38.6)),
+                  (95 , height - (30))]
 
-    coord_rhombus = [(225, height - (40 + rigid)),
-                  (250 + rigid,height - 25),
-                  (225, height - (10 - rigid)),
-                  (200 - rigid, height - 25)]
+    coord_rhombus = [(225, height - (40)),
+                  (250 ,height - 25),
+                  (225, height - (10 )),
+                  (200 , height - 25)]
     poly1 = pg.draw.polygon(image, blue, coord_Polygon, 0)
     poly2 = pg.draw.polygon(image, blue, coord_rectangle, 0)
     poly3 = pg.draw.polygon(image, blue, coord_rhombus, 0)
-    circle = pg.draw.circle(image, blue, (225, height - 125), 25 + rigid, 0)
-    ellipse = pg.draw.ellipse(image, blue, (150, height - 100, 80 + rigid, 40 + rigid), 0)
+    circle = pg.draw.circle(image, blue, (225, 50), 25, 0)
+    ellipse = pg.draw.ellipse(image, blue, (110, 80, 80 , 40 ), 0)
     Map_ = [poly1, poly2, poly3, circle, ellipse]
     return Map_
 
@@ -39,7 +39,7 @@ height = 200
 width = 300
 
 rigid = Radius + Clearance
-MAP = getMap(window,height,rigid)
+MAP = getMap(window,height)
 while True:
 
     for event in pg.event.get():
