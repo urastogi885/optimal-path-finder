f707a4e80d6e535aa1548fe50b761e0acbc87ac6
umang <umang.rastogi96@gmail.com>
2020-03-19 00:33:23 -0400

    Add method parameter in arguments
    Give user authority to launch a-star or dijkstra

--- point_explorer.py
+++ point_explorer.py
@@ -8,7 +8,7 @@ from utils.obstacle_space import Map
 from utils.explorer import Explorer
 from utils.node import Node
 
-script, start_node_coords, goal_node_coords = argv
+script, start_node_coords, goal_node_coords, method = argv
 
 
 if __name__ == '__main__':
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     # Initialize the explorer class
     start_node_coords = tuple(ast.literal_eval(start_node_coords))
     goal_node_coords = tuple(ast.literal_eval(goal_node_coords))
-    explorer = Explorer(start_node_coords, goal_node_coords)
+    explorer = Explorer(start_node_coords, goal_node_coords, str(method))
     obstacle_map = Map(0, 0)
     # Check validity of start and goal nodes
-    if not (obstacle_map.check_node_validity(start_node_coords[0], obstacle_map.height - start_node_coords[1] - 1)
-            and obstacle_map.check_node_validity(goal_node_coords[0], obstacle_map.height - goal_node_coords[1] - 1)):
+    if not (obstacle_map.check_node_validity(start_node_coords[0], obstacle_map.height - start_node_coords[1])
+            and obstacle_map.check_node_validity(goal_node_coords[0], obstacle_map.height - goal_node_coords[1])):
         print('One of the points lie in obstacle space!!\nPlease try again')
         quit()
     # Get the start node and add it to open nodes
@@ -36,10 +36,10 @@ if __name__ == '__main__':
         # Generate child nodes and iterate through them
         for child_node in current_node.generate_child_nodes((obstacle_map.width, obstacle_map.height)):
             node_repeated = False
-            # Update final weight of the child node
-            child_node.weight = explorer.get_final_weight(child_node.data, child_node.cost)
             # Make sure the node does not lie in the obstacle space
-            if obstacle_map.check_node_validity(child_node.data[0], obstacle_map.height - child_node.data[1] - 1):
+            if obstacle_map.check_node_validity(child_node.data[0], obstacle_map.height - child_node.data[1]):
+                # Update final weight of the child node
+                child_node.weight = explorer.get_final_weight(child_node.data, child_node.cost)
                 # Check for repetition of child node in closed nodes
                 for closed_node in explorer.closed_nodes:
                     if closed_node.data == child_node.data:
@@ -65,14 +65,16 @@ if __name__ == '__main__':
     # Get image of the map
     map_img = obstacle_map.get_map()
     blue = [255, 0, 0]
-    white = [0, 0, 0]
+    white = [255, 255, 255]
+    # Show all generated nodes
     for node in explorer.generated_nodes:
-        map_img[obstacle_map.height - node.data[1] - 1, node.data[0]] = white
+        map_img[obstacle_map.height - node.data[1], node.data[0]] = white
         imshow("Node Exploration", map_img)
         waitKey(1)
+    # Show path
     for data in path_data:
-        # print(data)
-        map_img[obstacle_map.height - data[1] - 1, data[0]] = blue
+        map_img[obstacle_map.height - data[1], data[0]] = blue
+    # Resize image to make it bigger and show it for 15 seconds
     map_img = resize(map_img, (obstacle_map.width * 2, obstacle_map.height * 2))
     imshow("Node Exploration", map_img)
     print('Exploration + Display Time:', time() - start_time)
