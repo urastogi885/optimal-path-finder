2166265f0967b2fc600c81578900d7e7495483a1
umang <umang.rastogi96@gmail.com>
2020-03-21 19:00:40 -0400

    Change method to check for obstacles

--- utils/obstacle_space.py
+++ utils/obstacle_space.py
@@ -2,21 +2,6 @@ import cv2
 import numpy as np
 
 
-def get_slopes(points):
-    points_length = len(points)
-    i = 0
-    slopes = []
-    while i < points_length:
-        if i != points_length - 1:
-            j = i + 1
-        else:
-            j = 0
-        slopes.append((points[j][1] - points[i][1]) / (points[j][0] - points[i][0]))
-        i += 1
-
-    return slopes
-
-
 class Map:
     def __init__(self, radius, clearance):
         deg_30 = np.pi / 6
@@ -45,85 +30,45 @@ class Map:
                                        (300 - 75 + (50 / 2), self.height - (30 / 2) - 10),
                                        (300 - 75, self.height - 10)],
                                       dtype=np.int32).reshape((-1, 2))
-        # Get slopes of all the edges of the convex polygon, rectangle, and rhombus
-        self.slopes_poly = get_slopes(self.coord_polygon)
-        self.slopes_rect = get_slopes(self.coord_rectangle)
-        self.slopes_rhom = get_slopes(self.coord_rhombus)
         # Define parameters of curved obstacles
         self.circle = [25, (225, 50)]
         self.ellipse = [(40, 20), (150, self.height - 100)]
-        # Define length of the convex polygon and the quadrilaterals
-        self.length_poly = len(self.coord_polygon)
-        self.length_quad = len(self.coord_rectangle)
-
-    def get_y_values(self, x, slopes, coordinates, edge_count):
-        dist = []
-        for i in range(edge_count):
-            if i < (edge_count / 2):
-                dist.append(slopes[i] * (x - coordinates[i][0]) + coordinates[i][1] - self.thresh)
-            else:
-                dist.append(slopes[i] * (x - coordinates[i][0]) + coordinates[i][1] + self.thresh)
-
-        return dist
-
-    def check_circle(self, x, y):
-        a = self.circle[1][0]
-        b = self.circle[1][1]
-        r = self.circle[0] + self.thresh
-
-        if (x - a) ** 2 + (y - b) ** 2 <= r ** 2:
-            return True
-
-        return False
-
-    def check_ellipse(self, x, y):
-        a = self.ellipse[0][0] + self.thresh
-        b = self.ellipse[0][1] + self.thresh
-
-        center_a = self.ellipse[1][0]
-        center_b = self.ellipse[1][1]
-
-        if ((x - center_a) / a) ** 2 + ((y - center_b) / b) ** 2 <= 1:
-            return True
-
-        return False
-
-    def check_polygons(self, x, y):
-        # Get y values for each edge of the convex polygon
-        y_poly = self.get_y_values(x, self.slopes_poly, self.coord_polygon, 6)
-        last_poly_slope = ((self.coord_polygon[2][1] - self.coord_polygon[5][1]) /
-                           (self.coord_polygon[2][0] - self.coord_polygon[5][0]))
-        y_poly.append(last_poly_slope * (x - self.coord_polygon[5][0]) + self.coord_polygon[5][1] + self.thresh)
-        # Get y values for each edge of the rectangle
-        y_rect = self.get_y_values(x, self.slopes_rect, self.coord_rectangle, 4)
-        # Get y values for each edge of the rhombus
-        y_rhom = self.get_y_values(x, self.slopes_rhom, self.coord_rhombus, 4)
-        # Return true if point lies within the convex polygon
-        if y_poly[0] <= y <= y_poly[6] and y_poly[1] <= y <= y_poly[5]:
-            return True
-        elif y_poly[2] <= y <= y_poly[4] and y_poly[6] <= y <= y_poly[3]:
-            return True
-        # Return true if point lies within the tilted rectangle
-        elif y_rect[0] <= y <= y_rect[2] and y_rect[1] <= y <= y_rect[3]:
-            return True
-        # Return true if point lies within the rhombus
-        elif y_rhom[0] <= y <= y_rhom[3] and y_rhom[1] <= y <= y_rhom[2]:
-            return True
-
-        return False
+        # Get image to search for obstacles
+        self.check_img = self.erode_image()
 
     def check_node_validity(self, x, y):
+        """
+        Method to check whether point lies within any obstacle
+        :param x: x-coordinate of the current node
+        :param y: y-coordinate of the current node
+        :return: false if point lies within any obstacle
+        """
         if x == self.width or y == self.height:
             return False
-        elif self.check_polygons(x, y) or self.check_circle(x, y) or self.check_ellipse(x, y):
+        elif self.check_img[y, x].all() == 0:
             return False
 
         return True
 
+    def erode_image(self):
+        """
+        Get eroded image to check for obstacles considering the robot radius and clearance
+        :return: image with obstacle space expanded to distance threshold between robot and obstacle
+        """
+        # Get map with obstacles
+        eroded_img = self.get_map()
+        # Erode map image for rigid robot
+        if self.thresh > 0:
+            kernel_size = (self.thresh * 2) + 1
+            erode_kernel = np.ones((kernel_size, kernel_size), np.uint8)
+            eroded_img = cv2.erode(eroded_img, erode_kernel, iterations=1)
+
+        return eroded_img
+
     def get_map(self):
         # Create empty image and fill it with white background
         img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
-        img.fill(200)
+        img.fill(255)
         # Define color as a tuple in BGR format for obstacles
         color = (0, 0, 0)
         # Display obstacle space using black color
