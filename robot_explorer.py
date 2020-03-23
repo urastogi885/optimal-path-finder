32102b26ad7da022ab0dbe61fe73a5b94d18f730
umang <umang.rastogi96@gmail.com>
2020-03-22 21:51:16 -0400

    Refactor code to use methods from explorer class
    Main is much cleaner

---
+++ robot_explorer.py
@@ -0,0 +1,48 @@
+# Import necessary standard libraries
+import ast
+from sys import argv
+from time import time
+# Import necessary custom-built classes and methods
+from utils.obstacle_space import Map
+from utils.explorer import Explorer, check_node_validity
+
+
+"""
+Add various parameters as input arguments from user
+:param start_node_data: a tuple of 2 values: start coordinates
+:param goal_node_data: a tuple of 2 values: goal coordinates
+:param robot_radius: radius of the robot
+:parma clearance: minimum distance between robot and any obstacle 
+:param method: d for dijkstra or a for a-star
+:param animation: 1 to show animation otherwise use 0
+"""
+script, start_node_coords, goal_node_coords, robot_radius, clearance, method, animation = argv
+
+if __name__ == '__main__':
+    # Convert arguments into their required data types and formats
+    start_node_coords = tuple(ast.literal_eval(start_node_coords))
+    start_node_coords = start_node_coords[1], start_node_coords[0]
+    goal_node_coords = tuple(ast.literal_eval(goal_node_coords))
+    goal_node_coords = goal_node_coords[1], goal_node_coords[0]
+    # Initialize the map class
+    obstacle_map = Map(int(robot_radius), int(clearance))
+    # Initialize the explorer class
+    explorer = Explorer(start_node_coords, goal_node_coords, str(method))
+    # Check validity of start and goal nodes
+    if not (check_node_validity(obstacle_map.check_img, start_node_coords[1], obstacle_map.height - start_node_coords[0])
+            and check_node_validity(obstacle_map.check_img, goal_node_coords[1], obstacle_map.height - goal_node_coords[0])):
+        print('One of the points lie in obstacle space!!\nPlease try again')
+        quit()
+    # Get start time for exploration
+    start_time = time()
+    # Start exploration
+    explorer.explore(obstacle_map.check_img)
+    # Show time for exploration
+    print('Exploration Time:', time() - start_time)
+    if int(animation) == 1:
+        # Get start time for animation
+        start_time = time()
+        # Display animation of map exploration to find goal
+        explorer.show_exploration(obstacle_map.get_map())
+        # Show time for animation
+        print('Animation Time:', time() - start_time)
